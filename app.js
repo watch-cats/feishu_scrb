6141f736629d81acdd2421950668aeae2c4b936f
watch-cats <luffy0607@foxmail.com>
2021-02-26 15:13:09 +0800

    done

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 App({
   onLaunch: function () {
+    var that = this;
+    tt.request({
+      url: 'http://10.0.1.21:8001',
+      data: {
 
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log('request 调用成功 ${res}')
+      },
+      fail(res) {
+        console.log('request 调用失败')
+      }
+    });
   }
 })
